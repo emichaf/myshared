bb5c75876de0eb43ee034d9004a57b41f3b8fdd5
emichaf <michael.frick@ericsson.com>
2018-03-09 17:02:43 +0100

    m

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,11 +1,13 @@
 def call(def server, def port) {
-    httpRequest httpMode: 'POST', url: "http://${server}:${port}/shutdown", validResponseCodes: '200,408'
-    sshagent(['RemoteCredentials']) {
-        sh "scp target/*.jar root@${server}:/opt/jenkins-demo.jar"
-        sh "ssh root@${server} nohup java -Dserver.port=${port} -jar /opt/jenkins-demo.jar &"
-    }
-    retry (3) {
-        sleep 5
-        httpRequest url:"http://${server}:${port}/health", validResponseCodes: '200', validResponseContent: '"status":"UP"'
-    }
+    
+	
+	withCredentials([[$class: 'UsernamePasswordMultiBinding',
+                                              credentialsId: 'myuser',
+                                              usernameVariable: 'myuser_USER',
+                                              passwordVariable: 'myuser_PASSWORD']]) {
+
+			
+    sh "sshpass -p ${myuser_PASSWORD} scp /target/*.jar ${myuser_USER}@${developmentServer}:/home/emichaf/myjarbuild.jar"
+			
+	
 }
