65b54bb3fc3b3621dc0dcb166730798c22080410
emichaf <michael.frick@ericsson.com>
2018-03-12 10:39:19 +0100

    m

--- vars/mypipeline.groovy
+++ vars/mypipeline.groovy
@@ -1,6 +1,6 @@
 //@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7')
-/*@Grab('org.apache.commons:commons-math3:3.4.1')
-import org.apache.commons.math3.primes.Primes*/
+/*@Grab('org.apache.commons:commons-math3:3.4.1')*/
+import org.apache.commons.math3.primes.Primes
 import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
 
 
