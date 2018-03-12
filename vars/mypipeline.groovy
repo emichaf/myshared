c3ced52fe5f3518595a1a2e3516b97705612d6db
emichaf <michael.frick@ericsson.com>
2018-03-12 10:54:39 +0100

    m

--- vars/mypipeline.groovy
+++ vars/mypipeline.groovy
@@ -1,7 +1,28 @@
 import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
 
+def minfunc(DOCKER_HOST) {
 
-def minfunc(Map DOCKER_HOST) {
+    unstash "eiffel-intelligence-artifact-wrapper"
+    try {
+
+        docker.withServer("$DOCKER_HOST", 'remote_docker_host') {
+            docker.image('emtrout/nind23').inside("--privileged") {
+                println "in stage"
+
+                sh "echo ${DOCKER_HOST}"
+
+
+            }
+        }
+
+    } catch (FlowInterruptedException interruptEx) {
+
+
+    }
+}
+
+
+def minfuncmap(Map DOCKER_HOST) {
 
     unstash "eiffel-intelligence-artifact-wrapper"
     try {
