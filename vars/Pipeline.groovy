2847f024adab1722a6d0c3aacb4cfdd504a31364
emichaf <michael.frick@ericsson.com>
2018-03-12 09:37:46 +0100

    m

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -1,7 +1,7 @@
 import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
 
 
-def call(DOCKER_HOST) {
+def min(DOCKER_HOST) {
 
     unstash "eiffel-intelligence-artifact-wrapper"
     try {
