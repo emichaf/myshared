4f62258720e672ce9a54fcd7a944b868d5803a9e
emichaf <michael.frick@ericsson.com>
2018-03-12 10:34:54 +0100

    m

--- vars/mypipeline.groovy
+++ vars/mypipeline.groovy
@@ -3,7 +3,7 @@ import org.apache.commons.math3.primes.Primes
 import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
 
 
-void minfunc(DOCKER_HOST) {
+void minfunc(String DOCKER_HOST) {
 
     unstash "eiffel-intelligence-artifact-wrapper"
     try {
