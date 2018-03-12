4b9ffee930b9259857ae2c63da76b6c00b44f063
emichaf <michael.frick@ericsson.com>
2018-03-12 09:27:38 +0100

    m

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -1,7 +1,7 @@
 import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
 
 
-def testar() {
+def call() {
     String DOCKER_HOST = "tcp://docker104-eiffel999.lmera.ericsson.se:4243"
 
     unstash "eiffel-intelligence-artifact-wrapper"
@@ -20,81 +20,5 @@ def testar() {
 
     }
 }
-
-
-def SC_1(DOCKER_HOST, BUILD_COMMAND) {
-
-
-    //String DOCKER_HOST = "tcp://docker104-eiffel999.lmera.ericsson.se:4243"
-    //String BUILD_COMMAND = "mvn clean package -DskipTests"
-
-    unstash "eiffel-intelligence"
-
-    try {
-
-        docker.withServer("$DOCKER_HOST", 'remote_docker_host') {
-            docker.image('emtrout/nind23').inside("--privileged") {
-                stage('XXStage GroovyX') {
-                    println "in stageXXX"
-
-                    // testar shared libs in local lib
-                    def shellLib = new shell()
-                    def commitId = shellLib.pipe("git rev-parse HEAD")
-                    println commitId
-
-
-
-                    sh "${BUILD_COMMAND}"
-                    sh "ls"
-
-
-                }
-            }
-        }
-    } catch (FlowInterruptedException interruptEx) {
-
-
-    }
-}
-
-
-def mybuildstep() {
-
-
-    String DOCKER_HOST = "tcp://docker104-eiffel999.lmera.ericsson.se:4243"
-    String BUILD_COMMAND = "mvn clean package -DskipTests"
-
-    unstash "eiffel-intelligence-artifact-wrapper"
-
-    println "mybuildstep was here!"
-
-
-    try {
-
-        docker.withServer("$DOCKER_HOST", 'remote_docker_host') {
-            docker.image('emtrout/nind23').inside("--privileged") {
-                stage('XXStage GroovyX') {
-                    println "in stageXXX"
-
-                    // testar shared libs in local lib
-                    def shellLib = new shell()
-                    def commitId = shellLib.pipe("git rev-parse HEAD")
-                    println commitId
-
-
-
-                    sh "${BUILD_COMMAND}"
-                    sh "ls"
-
-
-                }
-            }
-        }
-    } catch (FlowInterruptedException interruptEx) {
-
-
-    }
-}
-
 
 return this
