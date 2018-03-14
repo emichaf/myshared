1e20c3949cd7548e9ed4483a83af3dad35a98329
emichaf <michael.frick@ericsson.com>
2018-03-14 17:46:44 +0100

    m

--- vars/eventhandling.groovy
+++ vars/eventhandling.groovy
@@ -2,7 +2,7 @@ def send(Map pipelineParams) {
 
     echo sh(returnStdout: true, script: 'env')
 
-    def response = sh(returnStdout: true, script: "curl -H 'Content-Type: application/json' -X POST --data-binary '${pipelineParams.JSON_IN}' ${EVENT_PARSER_PUB_GEN_URI}${pipelineParams.EVENT_TYPE_IN}").trim()
+    def response = sh(returnStdout: true, script: "curl -H 'Content-Type: application/json' -X POST --data-binary '${pipelineParams.JSON_IN}' ${EIFFEL_EVENT_PARSER_PUB_GEN_URI}${pipelineParams.EVENT_TYPE_IN}").trim()
 
     sh "echo ${response}"
     response = readJSON text: "${response}"
