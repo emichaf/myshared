593404313c94a29d88ef67b56c4078d91ac666d1
emichaf <michael.frick@ericsson.com>
2018-03-14 09:31:53 +0100

    m

--- vars/parseSendEvent.groovy
+++ vars/parseSendEvent.groovy
@@ -1,5 +1,7 @@
 def parseSendEvent(json, EVENT_TYPE) {
 
+    def EVENT_PARSER_PUB_GEN_URI = 'http://docker104-eiffel999.lmera.ericsson.se:9900/doit/?msgType='
+
     echo sh(returnStdout: true, script: 'env')
 
     return sh(returnStdout: true, script: "curl -H 'Content-Type: application/json' -X POST --data-binary '${json}' ${EVENT_PARSER_PUB_GEN_URI}${EVENT_TYPE}").trim()
